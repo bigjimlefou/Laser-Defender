30625473a7f6345fff6039b3dc20c463fe6e3f98
bigjimlefou <cedric@sougne.name>
2020-05-28 17:58:05 +0200

    89. ViewPortToWorldPoint()

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,32 @@ using UnityEngine.EventSystems;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float paddingTop = 15f;
+    [SerializeField] private float paddingRight = 15f;
+    [SerializeField] private float paddingBottom = 15f;
+    [SerializeField] private float paddingLeft = 15f;
+
+    private float xMin;
+    private float xMax;
+    private float yMin;
+    private float yMax;
 
     // Start is called before the first frame update
     void Start()
     {
+        SetBoundaries();
+    }
+
+    private void SetBoundaries()
+    {
+        var mainCamera = Camera.main;
+        if (mainCamera != null)
+        {
+            xMin = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x+paddingLeft;
+            xMax = mainCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x-paddingRight;
+            yMin = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y+paddingBottom;
+            yMax = mainCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y-paddingTop;
+        }
     }
 
     // Update is called once per frame
@@ -22,8 +44,12 @@ public class Player : MonoBehaviour
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newXPosition = transform.position.x + deltaX;
-        var newYPosition = transform.position.y + deltaY;
-        transform.position = new Vector2(newXPosition, newYPosition);
+
+        var position = transform.position;
+        var newXPosition = Mathf.Clamp(position.x + deltaX, xMin, xMax);
+        var newYPosition = Mathf.Clamp(position.y + deltaY, yMin, yMax);
+
+        position = new Vector2(newXPosition, newYPosition);
+        transform.position = position;
     }
 }
