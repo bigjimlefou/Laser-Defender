cf6b0e18cf4324a9152e87d14198e5c0210e6567
bigjimlefou <cedric@sougne.name>
2020-07-15 16:37:12 +0200

    97. WaveConfig Scriptable Object

---
+++ Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject
+{
+    
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private GameObject pathPrefab;
+    [SerializeField] private float timeBetweenSpawns = 0.5f;
+    [SerializeField] private float spawnRandomFactor = 0.3f;
+    [SerializeField] private int numberOfEnemies;
+    [SerializeField] private float moveSpeed = 0.2f;
+
+    public GameObject EnemyPrefab => enemyPrefab;
+    public GameObject PathPrefab => pathPrefab;
+    public float TimeBetweenSpawns => timeBetweenSpawns;
+    public float SpawnRandomFactor => spawnRandomFactor;
+    public int NumberOfEnemies => numberOfEnemies;
+    public float MoveSpeed => moveSpeed;
+
+}
